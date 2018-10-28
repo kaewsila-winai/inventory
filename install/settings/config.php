091d8f64c3dc88453f60192cda99b1dc437cd135
admin <admin@goragod.com>
2018-10-28 12:56:30 +0700

    upgrade to 2.0.0

--- install/settings/config.php
+++ install/settings/config.php
@@ -1,19 +1,20 @@
 <?php
+
 /* config.php */
-return array (
-  'web_title' => 'Repair',
-  'web_description' => 'ระบบบันทึกข้อมูลงานซ่อม',
-  'timezone' => 'Asia/Bangkok',
-  'member_status' => 
-  array (
-    0 => 'สมาชิก',
-    1 => 'ผู้ดูแลระบบ',
-    2 => 'ช่างซ่อม',
-  ),
-  'color_status' => 
-  array (
-    0 => '#259B24',
-    1 => '#FF0000',
-    2 => '#0000FF',
-  ),
+
+return array(
+    'version' => '2.0.0',
+    'web_title' => 'Repair',
+    'web_description' => 'ระบบบันทึกข้อมูลงานซ่อม',
+    'timezone' => 'Asia/Bangkok',
+    'member_status' => array(
+        0 => 'สมาชิก',
+        1 => 'ผู้ดูแลระบบ',
+        2 => 'ช่างซ่อม',
+    ),
+    'color_status' => array(
+        0 => '#259B24',
+        1 => '#FF0000',
+        2 => '#0000FF',
+    ),
 );
--- install/settings/database.php
+++ install/settings/database.php
@@ -1,19 +1,20 @@
 <?php
 
 /* settings/database.php */
+
 return array(
-  'mysql' => array(
-    'dbdriver' => 'mysql',
-    'username' => 'root',
-    'password' => '',
-    'dbname' => 'repair',
-    'prefix' => 'erepair',
-  ),
-  'tables' => array(
-    'user' => 'user',
-    'language' => 'language',
-    'category' => 'category',
-    'repair' => 'repair',
-    'inventory' => 'inventory',
-  )
+    'mysql' => array(
+        'dbdriver' => 'mysql',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'repair',
+        'prefix' => 'erepair',
+    ),
+    'tables' => array(
+        'user' => 'user',
+        'language' => 'language',
+        'category' => 'category',
+        'repair' => 'repair',
+        'inventory' => 'inventory',
+    ),
 );
