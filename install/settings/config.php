177ef616ce42454952d094983561cc289c322dd1
admin <admin@goragod.com>
2019-12-07 20:07:27 +0700

    update Core

--- install/settings/config.php
+++ install/settings/config.php
@@ -3,7 +3,7 @@
 /* config.php */
 
 return array(
-    'version' => '2.0.8',
+    'version' => '2.1.0',
     'web_title' => 'Repair',
     'web_description' => 'ระบบบันทึกข้อมูลงานซ่อม',
     'timezone' => 'Asia/Bangkok',
